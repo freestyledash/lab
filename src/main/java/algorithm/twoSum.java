e8550b3a8d38865caf7e4563ca524c80195885be
张言琦 <freestyledash7@gmail.com>
2019-04-09 21:25:07 +0800

    feat(add two sun):

---
+++ src/main/java/algorithm/twoSum.java
@@ -0,0 +1,46 @@
+package algorithm;
+
+import java.util.HashMap;
+
+/**
+ * @author zhangyanqi
+ * @since 1.0 2019-04-09
+ */
+public class twoSum {
+
+    //o(n)
+    public int[] twoSum1(int[] nums, int target) {
+        int l = nums.length;
+        int[] res = new int[2];
+        for(int i=0;i<l;i++){
+            for(int ii=i+1;ii<l;ii++){
+                int a=nums[i];
+                int b=nums[ii];
+                if(a+b == target){
+                    res[0]=i;
+                    res[1]=ii;
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+
+    //o(n)
+    public int[] twoSum2(int[] nums, int target) {
+        int[] res = new int[2];
+        HashMap<Integer, Integer> integerIntegerHashMap = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            integerIntegerHashMap.put(nums[i],i);
+        }
+        for(int i=0;i<nums.length;i++){
+            int want = target-nums[i];
+            Integer integer = integerIntegerHashMap.get(want);
+            if(integer!=null&&integer!=i){
+                res[0]=i;
+                res[i]=integer;
+            }
+        }
+        return res;
+    }
+}
