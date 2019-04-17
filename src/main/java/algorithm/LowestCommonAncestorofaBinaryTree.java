7bd4a58f016971ae1f7c6da3b4433e0a6f46ca80
张言琦 <freestyledash7@gmail.com>
2019-04-17 09:44:18 +0800

    feat(增加2道空题目):

---
+++ src/main/java/algorithm/LowestCommonAncestorofaBinaryTree.java
@@ -0,0 +1,29 @@
+package algorithm;
+
+/**
+ * @author zhangyanqi
+ * @since 1.0 2019-04-17
+ * <p>
+ * leetcode 236
+ */
+public class LowestCommonAncestorofaBinaryTree {
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    //todo
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        return null;
+    }
+
+
+}
--- src/main/java/algorithm/MaximumDepthofBinaryTree.java
+++ src/main/java/algorithm/MaximumDepthofBinaryTree.java
@@ -4,6 +4,7 @@ package algorithm;
  * @author zhangyanqi
  * @since 1.0 2019-04-15
  * https://mp.weixin.qq.com/s/g9RyWpDWfGCnvPvw4res4Q
+ * 104题目
  */
 public class MaximumDepthofBinaryTree {
 
