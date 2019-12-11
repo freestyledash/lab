0f11b4abe0c454c9f785e985fed65399047e5fd1
张言琦（晓琦） <xiaoqi.zyq@cainiao.com>
2019-12-11 15:24:06 +0800

    删除无用的文件

---
+++ src/main/java/feature/lamda/Main.java
@@ -0,0 +1,34 @@
+package feature.lamda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author xiaoqi.zyq@alibaba-inc.com
+ * @date 2019/11/22
+ * @Description todo
+ * important_dependency todo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<List<String>> lists = new ArrayList<>();
+        ArrayList<String> strings1 = new ArrayList<>();
+        strings1.add("1");
+        strings1.add("2");
+        ArrayList<String> strings2 = new ArrayList<>();
+        strings2.add("3");
+        strings2.add("4");
+        lists.add(strings1);
+        lists.add(strings2);
+        List<String> collect = lists.stream().flatMap(s -> {
+            System.out.println(s);
+            return s.stream();
+        }).collect(Collectors.toList());
+        System.out.println(collect);
+
+    }
+
+}
