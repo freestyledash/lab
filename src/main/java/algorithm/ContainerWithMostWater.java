5e9531a57fb53e55ca79c2b13de3617692543d09
张言琦（晓琦） <xiaoqi.zyq@cainiao.com>
2019-12-25 22:20:01 +0800

    modify

--- src/main/java/algorithm/ContainerWithMostWater.java
+++ src/main/java/algorithm/ContainerWithMostWater.java
@@ -11,11 +11,27 @@ package algorithm;
 public class ContainerWithMostWater {
 
     public int maxArea(int[] height) {
-        return 0;
+        int result = 0;
+        for (int index = 0; index < height.length - 1; index++) {
+            int currentHeight = height[index];
+            for (int leftIndex = index + 1; leftIndex < height.length; leftIndex++) {
+                int width = leftIndex - index;
+                int realHeight = Math.min(currentHeight, height[leftIndex]);
+                int area = width * realHeight;
+                if (area > result) {
+                    result = area;
+                }
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-
+        ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
+        int[] input = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int[] input2 = {1, 1};
+        int i = containerWithMostWater.maxArea(input2);
+        System.out.println(i);
     }
 
 }
