90354285c15bbb181ef7f920cf489b2af679471d
张言琦（晓琦） <xiaoqi.zyq@cainiao.com>
2019-12-25 19:37:15 +0800

    init two questions

---
+++ src/main/java/algorithm/ContainerWithMostWater.java
@@ -0,0 +1,21 @@
+package algorithm;
+
+/**
+ * 11. Container With Most Water
+ * Medium
+ *
+ * @author xiaoqi.zyq@alibaba-inc.com
+ * @date 2019/12/25
+ */
+
+public class ContainerWithMostWater {
+
+    public int maxArea(int[] height) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/algorithm/ZigZagConversion.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+/**
+ * leetCode 6
+ * medium
+ *
+ * @author xiaoqi.zyq@alibaba-inc.com
+ * @date 2019/12/25
+ */
+public class ZigZagConversion {
+
+    public static void main(String[] args) {
+
+    }
+}
