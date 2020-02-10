62d6b463cd6b10281419f11542f5dd306cdb2cbb
张言琦 <freestyledash7@gmail.com>
2020-02-10 23:11:06 +0800

    finish some

---
+++ src/main/java/algorithm/tree/WordSearchI.java
@@ -0,0 +1,16 @@
+package algorithm.tree;
+
+/**
+ * 79. Word Search
+ * https://leetcode.com/problems/word-search/
+ * Medium
+ *
+ * @author zhangyanqi
+ * @since 1.0 2020-02-10
+ */
+public class WordSearchI {
+
+    public boolean exist(char[][] board, String word) {
+        return false;
+    }
+}
