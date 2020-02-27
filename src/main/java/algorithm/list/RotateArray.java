4dc865707a560250e4ce53df8257c9abf447b298
张言琦 <freestyledash7@gmail.com>
2020-02-27 23:56:15 +0800

    add some new code

--- src/main/java/algorithm/list/RotateArray.java
+++ src/main/java/algorithm/list/RotateArray.java
@@ -12,21 +12,34 @@ package algorithm.list;
 public class RotateArray {
 
     public void rotate(int[] nums, int k) {
-        k =  k % nums.length;
-        for(int start = 0; start < nums.length; start++){
+        //确定k的取值规律
+        k = k % nums.length;
+        //分析特点：从一个点出发，最终会回到这个点上
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
             int p = start;
             int pV = nums[start];
-            do{
-                int nP = getNewPosition(nums.length,p,k);
-                int temp  = nums[nP];
+            do {
+                int nP = getNewPosition(nums.length, p, k);
+                int temp = nums[nP];
                 nums[nP] = pV;
                 p = nP;
                 pV = temp;
-            }while(start != p);
+                count++;
+            } while (start != p);
         }
     }
 
-    private int getNewPosition(int length, int originPostion,int k){
-        return ( originPostion + k ) % length;
+    private int getNewPosition(int length, int originPosition, int k) {
+        return (originPosition + k) % length;
+    }
+
+    public static void main(String[] args) {
+        RotateArray rotateArray = new RotateArray();
+        int[] i = new int[]{1, 2, 3};
+        rotateArray.rotate(i, 1);
+        for (int m : i) {
+            System.out.println(m);
+        }
     }
 }
