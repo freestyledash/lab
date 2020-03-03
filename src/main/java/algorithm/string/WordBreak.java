60e11da83683ab18ff3465822af74ee8c50feb24
张言琦 <freestyledash7@gmail.com>
2020-03-03 20:35:31 +0800

    finish Word Breadk

--- src/main/java/algorithm/string/WordBreak.java
+++ src/main/java/algorithm/string/WordBreak.java
@@ -1,7 +1,6 @@
 package algorithm.string;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 139. Word Break
@@ -10,7 +9,7 @@ import java.util.List;
  * <p>
  * 思路:
  * BFS 耗时间
- * DP
+ * DP https://leetcode.com/problems/word-break/discuss/523665/Java-DP-solution-with-explanation
  *
  * @author zhangyanqi
  * @since 1.0 2020-03-02
@@ -20,6 +19,8 @@ public class WordBreak {
 
     //BF BFS bad
     public boolean wordBreak(String s, List<String> wordDict) {
+        wordDict.sort((e1, e2) -> e1.length() - e2.length());
+        System.out.println(wordDict);
         for (String word : wordDict) {
             if (isMatch(s, word)) {
                 if (s.length() == word.length()) {
@@ -49,14 +50,44 @@ public class WordBreak {
         return false;
     }
 
+
+    /**
+     * 动态规划完成
+     *
+     * @param s
+     * @param wordDict
+     * @return
+     */
+    public boolean wordBreakDp(String s, List<String> wordDict) {
+        //dp 的每个位置代表了在这里切一刀下去，能否match dict
+        boolean[] dp = new boolean[s.length() + 1];
+        Arrays.fill(dp, false);
+        dp[0] = true;
+
+
+        for (int cutPoint = 0; cutPoint < dp.length; cutPoint++) {
+            for (String word : wordDict) {
+                int i = cutPoint - word.length();
+                if (i >= 0 && dp[i] && isMatch(s.substring(i, cutPoint), word)) {
+                    dp[cutPoint] = true;
+                }
+            }
+        }
+        for (boolean a : dp) {
+            System.out.println(a);
+        }
+
+        return dp[s.length()];
+    }
+
+
     public static void main(String[] args) {
         WordBreak wordBreak = new WordBreak();
-        List<String> s = new ArrayList<String>();
-        s.add("a");
-        s.add("b");
-        s.add("c");
-        boolean abc = wordBreak.wordBreak("abc", s);
+        List<String> list = Arrays.asList("cats", "dog", "sand", "and", "cat");
+//        boolean abc = wordBreak.wordBreak("catsandog", list);
+        boolean abc = wordBreak.wordBreakDp("catsandog", list);
         System.out.println(abc);
     }
+
 
 }
