b86799688000a0c65fc57a6b88ddc33057f230e7
张言琦（晓琦） <xiaoqi.zyq@cainiao.com>
2020-03-05 00:59:42 +0800

    aadd explaination

--- src/main/java/algorithm/dp/BestTimeToBuyAndSellStockWithCoolDown.java
+++ src/main/java/algorithm/dp/BestTimeToBuyAndSellStockWithCoolDown.java
@@ -18,6 +18,34 @@ import org.junit.jupiter.api.Test;
  */
 public class BestTimeToBuyAndSellStockWithCoolDown {
 
+
+    /*
+
+    Solution
+
+    maintain two dp array buys and sells, and delta in prices between two days
+        buys[i] = max profit when buy stock on day i
+        sells[i] = max profit when sell stock on day i
+        delta = prices[i] - prices[i-1]
+    update sells[i]
+        initialize: sells[i] = 0 because no stock to sell at day 0
+        when sell stock on day i, [day i-1, day i] could be [buy, sell] or [do nothing, sell]
+            if [buy, sell], then val1 = buys[i-1] + prices[i] because we get prices[i] after selling the stock
+            if [do nothing, sell], it means that we regret selling stock on previous day and thus val2 = sells[i-1] + delta
+            thus sells[i] = max(val1, val2)
+    update buys[i]
+        initialize: buys[i] = -prices[0] because gain negative profit due to buying a stock
+        when buy stock on day i, [day i-2, day i-1, day i] could be [sell, cooldown, buy] or [sth, do nothing, buy]
+            if [sell, cooldown, buy], then val1 = sells[i-2] - prices[i] because we need to pay prices[i-1] to buy the stock
+            if [sth, do nothing, buy], it means that we regret buying stock on previous day and thus val2 = buys[i-1] - delta
+            thus buys[i] = max(val1, val2)
+    final max profit should be max(sells)
+
+
+
+
+     */
+
     public int maxProfit(int[] prices) {
         if (prices == null || prices.length == 1) {
             return 0;
