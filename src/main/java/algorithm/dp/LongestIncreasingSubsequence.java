9585e8f15edeadbbaa3d11e6fec1e9da7636732a
张言琦 <freestyledash7@gmail.com>
2020-03-16 23:14:20 +0800

    update

---
+++ src/main/java/algorithm/dp/LongestIncreasingSubsequence.java
@@ -0,0 +1,45 @@
+package algorithm.dp;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * 300. Longest Increasing Subsequence
+ * https://leetcode.com/problems/longest-increasing-subsequence/submissions/
+ * Medium
+ * todo
+ *
+ * @author zhangyanqi
+ * @since 1.0 2020/3/16
+ */
+public class LongestIncreasingSubsequence {
+
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] < nums[i]) {
+                dp[i] = dp[i - 1] + 1;
+            } else {
+                dp[i] = 1;
+            }
+        }
+        int result = 0;
+        for (int i : dp) {
+            result = Math.max(i, result);
+        }
+        System.out.println(Arrays.toString(dp));
+        return result;
+    }
+
+    @Test
+    public void test() {
+        int i = lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18});
+        System.out.println(i);
+    }
+}
