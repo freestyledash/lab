76fbfd242e2a0285059a4d450643277528534445
张言琦 <freestyledash7@gmail.com>
2020-03-19 22:07:37 +0800

    update

---
+++ src/main/java/algorithm/list/PalindromeNumber.java
@@ -0,0 +1,37 @@
+package algorithm.list;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 9. Palindrome Number
+ * https://leetcode.com/problems/palindrome-number/
+ * Easy
+ *
+ * 这也太简单了吧~
+ * 哈哈哈哈
+ *
+ * @author zhangyanqi
+ * @since 1.0 2020/3/19
+ */
+public class PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        String s = String.valueOf(x);
+        for (int i = 0; i < (s.length() / 2); i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    @Test
+    public void test() {
+        boolean palindrome = this.isPalindrome(1121);
+        System.out.println(palindrome);
+    }
+}
--- src/main/java/feature/reference/Main.java
+++ src/main/java/feature/reference/Main.java
@@ -3,24 +3,12 @@ package feature.reference;
 import java.lang.ref.*;
 
 /**
+ * 引用
+ *
  * @author zhangyanqi
  * @since 1.0 2019-02-08
  */
 public class Main {
-
-
-    /**
-     *
-     */
-    public static void weakReference() {
-        Object o = new Object();
-        WeakReference weakReference = new WeakReference(o);
-        o = null;
-        System.gc();
-        Object o1 = weakReference.get();
-        System.out.println(o1);  //null
-    }
-
 
     /**
      * gc when lack of Mem
@@ -34,6 +22,18 @@ public class Main {
         System.out.println(o1);  //not null
     }
 
+    /**
+     * not prevent gc
+     */
+    public static void weakReference() {
+        Object o = new Object();
+        WeakReference weakReference = new WeakReference(o);
+        o = null;
+        System.gc();
+        Object o1 = weakReference.get();
+        System.out.println(o1);  //null
+    }
+
 
     /**
      * didn't influence left cycle of gc ,only  get notification when gc
@@ -41,13 +41,15 @@ public class Main {
     public static void phantomReference() {
         ReferenceQueue referenceQueue = new ReferenceQueue();
         Object o = new Object();
-        PhantomReference<Object> objectSoftReference = new PhantomReference(o, referenceQueue);
-        Object o1 = objectSoftReference.get();
-        System.out.println(o1);  //must null whenever circumstance
+        PhantomReference phantomReference = new PhantomReference(o, referenceQueue);
+        Object o1 = phantomReference.get();
+        //!! must null whenever circumstance
+        System.out.println(o1);
+        //make sure o must be gc
         o = null;
         System.gc();
         try {
-            Reference remove = referenceQueue.remove();
+            Reference remove = referenceQueue.remove();// block until
             System.out.println(remove);  //after gc,is not null;before gc ,is null
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -55,8 +57,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-
-//        softReference();
         phantomReference();
     }
 }
