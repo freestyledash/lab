13e0807d4aee64bdcae412bc6ab9dde177e6b557
freestyledash <freestyledash7@gmail.com>
2021-03-14 17:30:06 +0800

    finish

--- src/main/java/algorithm/岛屿数量.java
+++ src/main/java/algorithm/岛屿数量.java
@@ -4,8 +4,7 @@ package algorithm;
  * Description
  * https://leetcode-cn.com/problems/number-of-islands/
  *
- * ImportantDependency
- * todo
+ * 搜索
  *
  * @author xiaoqi.zyq@alibaba-inc.com
  * @date 2021/03/13
@@ -14,7 +13,41 @@ public class 岛屿数量 {
 
     public int numIslands(char[][] grid) {
 
-        //todo
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    //开始搜索 DFS
+                    search(grid, i, j);
+                    result++;
+                }
+
+            }
+        }
+
+        return result;
+    }
+
+    void search(char[][] grid, int x, int y) {
+        if (!(x >= 0 && grid.length > x && y >= 0 && grid[x].length > y)) {
+            return;
+        }
+        if (grid[x][y] == '1') {
+            //清理
+            grid[x][y] = '0';
+
+            //dfs
+            search(grid, x + 1, y);
+            search(grid, x - 1, y);
+            search(grid, x, y + 1);
+            search(grid, x, y - 1);
+        }
+
     }
 
 }
