4ff6996182172f52c31742ce112fd5ae858a6586
张言琦 <yanqi.zhang@viatris.com>
2022-08-16 18:14:58 +0800

    mybatis plus generator

--- src/main/java/feature/mybatisplus/generator/Generator.java
+++ src/main/java/feature/mybatisplus/generator/Generator.java
@@ -11,27 +11,9 @@ import java.util.HashMap;
 public class Generator {
 
     public static void main(String[] args) {
-
-        String tablePrefix = "";
-
+        
         //表名字
         String tableName = "test_table";
-
-        Config config = new Config();
-
-        config.setDbType(DbType.MYSQL);
-        config.setDriver("com.mysql.jdbc.Driver");
-        config.setDbUrl("jdbc:mysql://127.0.0.1:3306/test_repo");
-        config.setUsername("root");
-        config.setPassword("123456");
-        config.setAuthor("张言琦");
-        config.setProjectPath("/Users/zhangyanqi/projects/lab");
-        config.setApplicationName("lab");
-        config.setModuleName("lab");
-        config.setParentName("feature.mybatisplus.biz");
-
-        //是否覆盖生产文件，默认false
-        config.setFileOverride(false);
 
         HashMap<OutputFile, String> pathInfo = new HashMap<>();
 
@@ -43,21 +25,22 @@ public class Generator {
 
         try {
 
-
             FastAutoGenerator.create(
-                            config.getDbUrl(),
-                            config.getUsername(),
-                            config.getPassword()
+                            "jdbc:mysql://127.0.0.1:3306/test_repo",
+                            "root",
+                            "123456"
                     )
 
                     .globalConfig(builder -> {
-                        builder.author(config.getAuthor()) // 设置作者
-                                .outputDir(config.getProjectPath()); // 指定输出目录
+                        builder.author("张言琦") // 设置作者
+                                .outputDir("/Users/zhangyanqi/projects/lab"); // 指定输出目录
 
                     })
 
                     .packageConfig(builder -> {
-                        builder.parent(config.getParentName()) // 设置父包名
+                        // 设置父包名
+                        builder.parent("feature.mybatisplus.biz")
+                                .serviceImpl("feature.mybatisplus.biz.serviceimpl")
                                 .pathInfo(pathInfo);
                     })
 
