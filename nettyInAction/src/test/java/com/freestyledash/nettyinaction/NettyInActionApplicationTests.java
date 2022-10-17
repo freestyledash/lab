36f2ebd2d3bc9a88d118d25c990c6fed64946a0c
张言琦 <yanqi.zhang@viatris.com>
2022-10-17 16:22:52 +0800

    1

---
+++ nettyInAction/src/test/java/com/freestyledash/nettyinaction/NettyInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freestyledash.nettyinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
